d0bcce137021019a1c8d2e3d239d1390081450bf
Franklin Webber <franklin.webber@gmail.com>
2016-10-26 22:01:10 -0700

    Adds the guard being used and tested

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,29 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+# Normally this would be defined in the lib directory
+module MyCompany
+  module Helpers
+    def marker_file_exists?
+      File.exist?('/marker_file')
+    end
+  end
+end
+
+# Normally this would be defined in the lib directory
+Chef::Resource::Execute.include MyCompany::Helpers
+
+execute('database_script') do
+  not_if do
+    # Using File as a class is hard to use with tests because if you
+    # override some of the default behaviors it will cause other issues
+    # when other processes like RSpec or Fauxhai load the data.
+
+    # File.exist?('/marker_file')
+
+    # The easier thing is to place a method around it which is easier to stub.
+    # This useful in declaring complicated guards more clearly as well.
+    marker_file_exists?
+  end
+end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -9,12 +9,36 @@ require 'spec_helper'
 describe 'guards::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '6.7')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
+
+    context 'when the marker file is present' do
+      before do
+        # allow(File).to receive(:exist?).with('/marker_file').and_return(true)
+        allow_any_instance_of(Chef::Resource::Execute).to receive(:marker_file_exists?).and_return(true)
+      end
+
+      it 'should not execute' do
+        resource = chef_run.execute('database_script')
+        expect(resource).to do_nothing
+      end
+    end
+
+    context 'when the marker file is missing' do
+      before do
+        # allow(File).to receive(:exist?).with('/marker_file').and_return(false)
+        allow_any_instance_of(Chef::Resource::Execute).to receive(:marker_file_exists?).and_return(false)
+      end
+
+      it 'should execute' do
+        expect(chef_run).to run_execute('database_script')
+      end
+    end
+
   end
 end
